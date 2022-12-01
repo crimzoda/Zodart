eaa7d2abdc7ab2f4e827692976d786428d6ec154
crimzoda <46323591+crimzoda@users.noreply.github.com>
2022-12-01 05:16:22 +0000

    Update refreshgallery.php

--- refreshgallery.php
+++ refreshgallery.php
@@ -1,22 +1,18 @@
 <?php
     //this is a cron job that refreshes the displayed gallery everyday
-	$conn = mysqli_connect("localhost", "username", "password", 'table') or die(mysqli_connect_error());
+	$conn = mysqli_connect("localhost:3306", "b7v3ht", "A636*nq0l", 't6fpn7') or die(mysqli_connect_error());
 	
     //the display from the previos day is cleared
 	$clearDisplayQuery = "DELETE FROM display";
     /*the auto increment is set back to one so that it doesn't
     keep adding from day to day*/
 	$resetIncrement = "ALTER table display AUTO_INCREMENT = 1";
-    $selectedItems = mysqli_query($conn, "SELECT title, url, author FROM gallery ORDER BY RAND() LIMIT 3");
     //3 rows are randomly inserted into the display table from the gallery table
-	$addDisplayQuery = "INSERT INTO display(title, url, author) $selectedItems";
-	/*removes the displayed items from the gallery to avoid repeat
-	submissions being displayed*/
-    $removeGalleryItems = "DELETE $selectedItems";
+	$addDisplayQuery = "INSERT INTO display(title, url, author) SELECT title, url, author FROM gallery ORDER BY RAND() LIMIT 3";
 
 	if (mysqli_query($conn, $clearDisplayQuery))
 	{
-		echo "Display cleared!";
+		echo "Submitted!";
 	}
 	else
 	{
@@ -25,7 +21,7 @@
 
 	if (mysqli_query($conn, $resetIncrement))
 	{
-		echo "Increment reset!";
+		echo "Submitted!";
 	}
 	else
 	{
@@ -34,16 +30,7 @@
 
 	if (mysqli_query($conn, $addDisplayQuery))
 	{
-		echo "Submissions added to display!";
-	}
-	else
-	{
-		echo "Oh no something happened, it didn't work..." . mysqli_error($conn);
-	}
-
-    if (mysqli_query($conn, $removeGalleryItems))
-	{
-		echo "Gallery submissions deleted!";
+		echo "Submitted!";
 	}
 	else
 	{
